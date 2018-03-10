55145a2eb1e46c315702802f8df38f76a0b66f8f
electronstudio <misc@electronstudio.co.uk>
2018-03-10 02:44:53 +0000

    refactor

---
+++ core/src/com/mygdx/game/ButtonMasherGame0.java
@@ -0,0 +1,100 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.Align;
+import org.jetbrains.annotations.NotNull;
+import uk.me.fantastic.retro.games.Player;
+import uk.me.fantastic.retro.games.SimpleGame;
+import uk.me.fantastic.retro.screens.GameSession;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * A simple RetroWar game
+ */
+
+public class ButtonMasherGame0 extends SimpleGame {
+
+    private String message = "";                        // We will print this to screen
+    private final boolean[] buttonMashed = new boolean[4];   // Remember if button was held down
+    private final Texture baloon = new Texture(Gdx.files.internal("ButtonMasherGame/baloon.png"));
+    private final Sprite[] sprites = {                         // An array of 4 sprites
+            new Sprite(baloon), new Sprite(baloon),
+            new Sprite(baloon), new Sprite(baloon)
+    };
+
+    private final Texture background = new Texture(Gdx.files.internal("ButtonMasherGame/sky4.png"));
+
+    private static final BitmapFont font = new BitmapFont(Gdx.files.internal("ButtonMasherGame/c64_low3_black.fnt"));   // for drawing text
+
+    public ButtonMasherGame0(GameSession session) {                     // Constructor (required)
+        super(session, 256, 320, font, font);              // width and height of screen in pixels
+        for (int i = 0; i < sprites.length; i++) {                      // Set initial positions for the sprites
+            sprites[i].setPosition(36 + i * 56, 33);
+        }
+    }
+
+    @Override
+    public void doLogic(float deltaTime) {  // Called automatically every frame
+        for (int i = 0; i < getPlayers().size(); i++) {
+            Sprite sprite = sprites[i];
+            Player player = getPlayers().get(i);
+            sprite.setY(sprite.getY() - deltaTime * 20);            // move sprite downwards
+            if (player.getInput().getA()) {              // if button is pressed
+                if (!buttonMashed[i]) {                             // prevent holding down button to cheat
+                    sprite.setY(sprite.getY() + deltaTime * 300);   // move sprite upwards
+                    buttonMashed[i] = true;
+                }
+            } else {
+                buttonMashed[i] = false;
+            }
+            if (sprite.getY() < 33) sprite.setY(33);                 // dont fall through floor
+            player.setScore((int) (sprite.getY()));  // score = height
+        }
+    }
+
+    @Override
+    public void doDrawing(@NotNull Batch batch) {            // called automatically every frame
+        Gdx.gl.glClearColor(0, 0, 0, 1);     // clear the screen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(background, 0, 0);
+        drawPlayers(batch);
+        font.draw(batch, message, 0, 312, 256, Align.center, false);
+        batch.end();
+    }
+
+
+    private void drawPlayers(Batch batch) {                 // draw one sprite for each Player
+        for (int i = 0; i < getPlayers().size(); i++) {
+            Sprite sprite = sprites[i];
+            Player player = getPlayers().get(i);
+            sprite.draw(batch);
+            font.draw(batch, player.getName(), sprite.getX() - 15, 8);
+        }
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        baloon.dispose();
+    }
+
+    // These methods must be implemented but don't have to do anything
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+
+}
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,51 +1,19 @@
 package com.mygdx.game.desktop;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.mygdx.game.ButtonMasherGame;
-import com.mygdx.game.MyTitleScreen;
+import com.mygdx.game.ButtonMasherGame0;
 import uk.me.fantastic.retro.App;
-import uk.me.fantastic.retro.SingleGameApp;
+import uk.me.fantastic.retro.SimpleApp;
 import uk.me.fantastic.retro.desktop.DesktopCallback;
-import uk.me.fantastic.retro.games.AbstractGameFactory;
-import uk.me.fantastic.retro.games.GameFactory;
-import uk.me.fantastic.retro.screens.GameSession;
-import uk.me.fantastic.retro.screens.GenericTitleScreen;
+import uk.me.fantastic.retro.games.SimpleGameFactory;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		
-		
 		DesktopCallback callback = new DesktopCallback();
-		
-		
-//		App app = new App(callback){
-//			@Override
-//			public void start(){
-//				GameFactory factory = new GameFactory("ButtonMasher", (GameSession s) -> new ButtonMasherGame(s), null);
-//
-//				GameSession session = new GameSession(factory, null);
-//				//session.setGame(new ButtonMasherGame(session));
-//				setScreen(session);
-//			}
-//		};
-
-		//Texture texture = new Texture(Gdx.files.internal("badlogic.jpg"));
-
-	//	GameFactory factory = new GameFactory("ButtonMasher", (GameSession s) -> new ButtonMasherGame(s), null);
-
-		//ArrayList<AbstractGameFactory> games = new ArrayList<AbstractGameFactory>();
-
-	//	games.add(factory);
-		App app = new SingleGameApp(callback, "Button Masher", ButtonMasherGame.class, MyTitleScreen.class, null);
-
-
-		new Lwjgl3Application(app, callback.getConfig());
-		
-
+		SimpleGameFactory factory = new SimpleGameFactory("Button Masher", ButtonMasherGame0.class);
+		App app = new SimpleApp(callback, "Button Masher", factory);
+		new LwjglApplication(app, callback.getConfig());
 	}
 }
